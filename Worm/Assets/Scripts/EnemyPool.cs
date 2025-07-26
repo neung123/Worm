116ec7490d85bdf0c2632d05281dfa1c85aa5e84
neung pornpavee <pornpavee.s@fairplaystudios.net>
2025-07-26 20:27:22 +0700

    controller

---
+++ Worm/Assets/Scripts/EnemyPool.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : MonoBehaviour
+{
+    private Enemy _enemyPrefab;
+    private Queue<Enemy> _pool = new Queue<Enemy>();
+
+    public void Init(Enemy enemy)
+    {
+        _enemyPrefab = enemy;
+    }
+
+    public Enemy GetEnemy()
+    {
+        if (_pool.Count == 0)
+        {
+            Enemy newEnemy = Instantiate(_enemyPrefab);
+            newEnemy.gameObject.SetActive(false);
+            _pool.Enqueue(newEnemy);
+        }
+
+        Enemy enemyToUse = _pool.Dequeue();
+        enemyToUse.gameObject.SetActive(true);
+        return enemyToUse;
+    }
+
+    public void ReturnEnemy(Enemy enemy)
+    {
+        enemy.gameObject.SetActive(false);
+        _pool.Enqueue(enemy);
+    }
+}
